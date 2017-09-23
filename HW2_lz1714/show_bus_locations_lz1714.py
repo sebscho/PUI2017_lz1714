41f391d5ea6bd7ca023c5f0da23d00860e6f0a86
Lingyi Zhang <Lingyi@Lingyis-MBP.fios-router.home>
2017-09-23 13:59:27 -0400

    HW2

--- HW2_lz1714/show_bus_locations_lz1714.py
+++ HW2_lz1714/show_bus_locations_lz1714.py
@@ -1,26 +1,33 @@
+'''Input: This python script takes in the API key and the bus line number.
+   Output: bus line number, number of active buses, each bus's latitude and longitude.'''
 from __future__ import print_function
 import sys
+import os
 import json
 try:
     import urllib2 as urllib
 except ImportError:
     import urllib.request as urllib
-import os
 
 # make sure this file takes exactly 2 arguments
 if not len(sys.argv) == 3:
     print ("Invalid number of arguments. Run as: python show_bus_locations_lz1714.py <MTA_KEY> <BUS_LINE>")
     sys.exit()
 
-# fetch and open the data from MTA using MTA_KEY
+# fetch and open the data from MTA through the SIRI API.
+# set the API KEY to an environment variable so that other people can use their
+# own key and the same environment varialble name to read the data.
 apiKey = os.getenv(sys.argv[1])
-url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=" + \
-    apiKey + "&VehicleMonitoringDetailLevel=calls&LineRef=" + sys.argv[2]
+url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=" + apiKey + "&VehicleMonitoringDetailLevel=calls&LineRef=" + sys.argv[2]
 response = urllib.urlopen(url)
 data = response.read()
 data = json.loads(data)
+
+# locate vehicle information
 content_siri_serv_vehi = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery']
 activity = content_siri_serv_vehi[0]['VehicleActivity']
+
+# output 
 print ("Bus Line :", sys.argv[2])
 print ("Number of Active Buses :", len(activity))
 for i in range(len(activity)):
